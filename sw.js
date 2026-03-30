1e8768d6acd66ea0dc6f284a1d43c31312275d77
jvzorrillamartinez-web <jvzorrillamartinez@gmail.com>
2026-03-30 13:05:32 +0200

    Remove service worker functionality
    Remove service worker event listeners for install and fetch.

--- sw.js
+++ sw.js
@@ -1,16 +1,1 @@
-self.addEventListener('install', (e) => {
- e.waitUntil(
-   caches.open('senda-store').then((cache) => cache.addAll([
-     'index.html',
-     'style.css',
-     'app.js',
-     'manifest.json'
-   ])),
- );
-});
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
-  );
-});
